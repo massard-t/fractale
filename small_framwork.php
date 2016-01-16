4919ac38088e0035269436c5cd41618bb0550784
Bob <theomassard@hotmail.fr>
2016-01-16 16:29:17 +0100

    ajout description fonctions

--- small_framwork.php
+++ small_framwork.php
@@ -1,19 +1,19 @@
 <?php
 //THANKS TO MY BUDDY MATTHEW FOR HELPING ME LIKE A FAG
-function cmod($reel, $imaginaire)
+function cmod($reel, $imaginaire) //CALCUL MODULE COMPLEX
 {
 	$module = sqrt($reel**2 + $imaginaire**2);
 	return ($module);
 }
 
-function cadd($c_a, $c_b)
+function cadd($c_a, $c_b) // CALCUL ADDITION COMPLEX
 {
 	$cadd = array('r' => ($c_a['r'] + $c_b['r']),
 				  'im' => ($c_a['im'] + $c_b['im']));
 	return ($cadd);
 }
 
-function cmul($c_a, $c_b)
+function cmul($c_a, $c_b) // CALCUL MULTIPLICATION COMPLEX
 {
 	$cmul = array(
 		'r' => (($c_a['r'] * $c_b['r']) - ($c_a['im'] * $c_b['im'])),
@@ -21,7 +21,7 @@ function cmul($c_a, $c_b)
 	return ($cmul);
 }
 
-function cpow($comp, $k)
+function cpow($comp, $k) // CALCUL PUISSANCE COMPLEX
 {
 	$crep = $comp;
 	for ($i=1; $i < $k; $i++) { 
@@ -30,7 +30,7 @@ function cpow($comp, $k)
 	return ($crep);
 }
 
-function is_mandelbrot($mandel)
+function is_mandelbrot($mandel) //CALCUL SI NOMBRE = ENSEMBLE MANDELBROT
 {
 	$cu = array('r' => 0,
 	            'im' => 0);
@@ -50,6 +50,3 @@ $man = array('r' => $argv[1], //FLOAT
 			 'im' => $argv[2],//FLOAT
 			 'k' => $argv[3], //INTEGER
 			 'n' => $argv[4]);//INTEGER
-
-print_r(is_mandelbrot($man));
-
