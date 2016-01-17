7809339c18175b53f8a70e8f731d9091d4965620
Deville Matthew <kemiimek@live.fr>
2016-01-17 17:20:39 +0100

    Relocalisation de la fonction mandelbrot

--- small_framwork.php
+++ small_framwork.php
@@ -94,21 +94,6 @@ function cmul($c_a, $c_b) // CALCUL MULTIPLICATION COMPLEX
 // 	return ($crep);
 // }
 
-function is_mandelbrot($mandel) //CALCUL SI NOMBRE = ENSEMBLE MANDELBROT
-{
-	$cu = array('r' => 0,
-	            'im' => 0);
-	$ca = array('r' => $mandel['r'],
-				'im' => $mandel['im']);
-	for ($i=0; $i < $mandel['n']; $i++) { 
-		$cu = cadd(cpow($cu, $mandel['k']), $ca);
-		if (cmod($cu['r'], $cu['im']) > 2) {
-			return (255); // RETURN WHITE BECAUSE NODELBROT
-		}
-	}
-	return (0); //RETURN BLACK BECAUSE MANDELBROOOOOT
-}
-
 $complex = new complex($argv[1], $argv[2]);
 $clx = new complex($argv[3], $argv[4]);
 $result = $complex->mul($complex);
