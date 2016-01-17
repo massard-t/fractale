f159b929633d20d2a5716d172d86f5a4f1fbe4b5
Deville Matthew <kemiimek@live.fr>
2016-01-17 17:31:19 +0100

    Update projet_mandelbrot.php

--- projet_mandelbrot.php
+++ projet_mandelbrot.php
@@ -4,7 +4,7 @@ function is_mandelbrot($mandel) //CALCUL SI NOMBRE = ENSEMBLE MANDELBROT
 	$cu = array('r' => 0,
 	            'im' => 0);
 	$ca = array('r' => $mandel['r'],
-				'im' => $mandel['im']);
+		    'im' => $mandel['im']);
 	for ($i=0; $i < $mandel['n']; $i++) { 
 		$cu = cadd(cpow($cu, $mandel['k']), $ca);
 		if (cmod($cu['r'], $cu['im']) > 2) {
@@ -14,6 +14,10 @@ function is_mandelbrot($mandel) //CALCUL SI NOMBRE = ENSEMBLE MANDELBROT
 	return (0); //RETURN BLACK BECAUSE MANDELBROOOOOT
 }
 
-$mandel = array($argv[1],
-		)
+$mandel = array('r' => $argv[1],
+		'im' => $argv[2],
+		'n' => $argv[3],
+		'k' => $argv[4]);
+		
+echo is_mandelbrot($mandel);
 ?>
